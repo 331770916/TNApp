ce9ec6e10fc11f2398f38d32a670080bdb12ba4f
zhangbiaoiao@live.com <zhangbiaoiao@live.com>
2017-06-22 16:53:53 +0800

    分级基金代码 Activity  布局 分级基金今日 Pager   布局

---
+++ app/src/main/java/com/tpyzq/mobile/pangu/activity/trade/stock/FJEntrustedQueryActivity.java
@@ -0,0 +1,21 @@
+package com.tpyzq.mobile.pangu.activity.trade.stock;
+
+import com.tpyzq.mobile.pangu.R;
+import com.tpyzq.mobile.pangu.base.BaseActivity;
+
+/**
+ * Created by zhang on 2017/6/22.
+ */
+
+public class FJEntrustedQueryActivity extends BaseActivity{
+
+    @Override
+    public void initView() {
+
+    }
+
+    @Override
+    public int getLayoutId() {
+        return R.layout.activity_fj_entrus_query;
+    }
+}
