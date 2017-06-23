52777360ae3daa7ac58c984ebd90ca9ea20cc3b4
zhangbiaoiao@live.com <zhangbiaoiao@live.com>
2017-06-23 09:08:49 +0800

    添加分级基金界面 添加查询 布局 添加分级基金选择界面  布局

---
+++ app/src/main/java/com/tpyzq/mobile/pangu/activity/trade/stock/FJFundChooseActivity.java
@@ -0,0 +1,18 @@
+package com.tpyzq.mobile.pangu.activity.trade.stock;
+
+import com.tpyzq.mobile.pangu.R;
+import com.tpyzq.mobile.pangu.base.BaseActivity;
+
+
+
+public class FJFundChooseActivity extends BaseActivity {
+    @Override
+    public void initView() {
+
+    }
+
+    @Override
+    public int getLayoutId() {
+        return R.layout.activity_choose_fund;
+    }
+}
---
+++ app/src/main/java/com/tpyzq/mobile/pangu/activity/trade/stock/FJFundGradingMergerActivity.java
@@ -0,0 +1,19 @@
+package com.tpyzq.mobile.pangu.activity.trade.stock;
+
+
+import com.tpyzq.mobile.pangu.R;
+import com.tpyzq.mobile.pangu.base.BaseActivity;
+
+public class FJFundGradingMergerActivity extends BaseActivity {
+
+
+    @Override
+    public void initView() {
+
+    }
+
+    @Override
+    public int getLayoutId() {
+        return R.layout.activity_grading_fund_merger;
+    }
+}
