2558235f5bb60eaee46919ed96bf3c05d393eed9
ltyhome <ltyhome@yahoo.com.hk>
2017-06-27 14:07:39 +0800

    修改接口类添加300704当日委托查询基金接口

--- app/src/main/java/com/tpyzq/mobile/pangu/activity/trade/view/FJEntrustDealQueryPager.java
+++ app/src/main/java/com/tpyzq/mobile/pangu/activity/trade/view/FJEntrustDealQueryPager.java
@@ -1,11 +1,18 @@
 package com.tpyzq.mobile.pangu.activity.trade.view;
+import android.app.Activity;
+import android.app.Dialog;
 import android.content.Context;
+import android.graphics.Color;
 import android.os.SystemClock;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.AbsListView;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.RelativeLayout;
+import android.widget.TextView;
+
 import com.handmark.pulltorefresh.library.PullToRefreshBase;
 import com.handmark.pulltorefresh.library.PullToRefreshListView;
 import com.tpyzq.mobile.pangu.adapter.trade.FJEntrustedDealListViewAdapter;
@@ -14,7 +21,10 @@ import com.tpyzq.mobile.pangu.R;
 import com.tpyzq.mobile.pangu.base.InterfaceCollection;
 import com.tpyzq.mobile.pangu.data.ResultInfo;
 import com.tpyzq.mobile.pangu.data.StructuredFundEntity;
-
+import com.tpyzq.mobile.pangu.view.dialog.LoadingDialog;
+import com.tpyzq.mobile.pangu.view.dialog.MistakeDialog;
+import com.tpyzq.mobile.pangu.view.pickTime.TimePickerView;
+import java.util.Date;
 import java.util.List;
 
 /**
@@ -23,13 +33,18 @@ import java.util.List;
  * Describe: entrust deal query 委托0 成交查询1
  */
 public class FJEntrustDealQueryPager extends BasePager implements InterfaceCollection.InterfaceCallback,FJEntrustedDealListViewAdapter.ScallCallback{
+    private TextView mtvStartTime,mtvFinishTime,fjInquire;
+    private String TAG,startDate,finishDate,position = "";
     private FJEntrustedDealListViewAdapter mAdapter;
+    private TimePickerView startTime,finishTime;
     private List<StructuredFundEntity> myList;
     private PullToRefreshListView listView;
+    private boolean isScallBottom,mIsClean;
+    private Dialog mistakeDialog,mDialog;
+    private LinearLayout fjTimepicker;
     private RelativeLayout kong_null;
     private ImageView iv_isEmpty;
-    private boolean isScallBottom;
-    private String TAG;
+    private int refresh = 30;
 
     public FJEntrustDealQueryPager(Context context,String params) {
         super(context,params);
@@ -42,20 +57,106 @@ public class FJEntrustDealQueryPager extends BasePager implements InterfaceColle
         listView.setMode(PullToRefreshBase.Mode.PULL_FROM_START);
         iv_isEmpty = (ImageView) rootView.findViewById(R.id.iv_isEmpty);
         kong_null = (RelativeLayout) rootView.findViewById(R.id.EAMP_Kong_Null);
+        if(TAG.equals("EntrustCustomPager")||TAG.equals("DealCustomPager")) {
+            fjTimepicker = (LinearLayout)rootView.findViewById(R.id.fjTimepicker);
+            fjTimepicker.setVisibility(View.VISIBLE);
+            mtvStartTime = (TextView)rootView.findViewById(R.id.fjstartDate);
+            mtvFinishTime = (TextView)rootView.findViewById(R.id.fjfinishDate);
+            fjInquire = (TextView)rootView.findViewById(R.id.fjInquire);
+            fjInquire.setOnClickListener(new MyOnClickListenr());
+            mtvStartTime.setText(helper.getCurDate());
+            mtvStartTime.setTextColor(Color.parseColor("#368de7"));
+            mtvStartTime.setOnClickListener(new MyOnClickListenr());
+            mtvFinishTime.setText(helper.getCurDate());
+            mtvFinishTime.setOnClickListener(new MyOnClickListenr());
+            mtvFinishTime.setTextColor(Color.parseColor("#368de7"));
+            startTime = new TimePickerView(mContext, TimePickerView.Type.YEAR_MONTH_DAY);
+            startTime.setTime(new Date());
+            startTime.setCyclic(false);
+            startTime.setCancelable(true);
+            startTime.setTitle("选择日期");
+            startTime.setOnTimeSelectListener(new TimePickerView.OnTimeSelectListener() {
+
+                @Override
+                public void onTimeSelect(Date date) {
+                    mtvStartTime.setText(simpleDateFormat.format(date));
+                }
+            });
+            finishTime = new TimePickerView(mContext, TimePickerView.Type.YEAR_MONTH_DAY);
+            finishTime.setTime(new Date());
+            finishTime.setCyclic(false);
+            finishTime.setCancelable(true);
+            finishTime.setTitle("选择日期");
+
+            //时间选择后回调
+            finishTime.setOnTimeSelectListener(new TimePickerView.OnTimeSelectListener() {
+
+                @Override
+                public void onTimeSelect(Date date) {
+                    mtvFinishTime.setText(simpleDateFormat.format(date));
+                }
+            });
+        }
     }
 
 
+    class MyOnClickListenr implements View.OnClickListener {
+
+        @Override
+        public void onClick(View view) {
+            switch (view.getId()) {
+                case R.id.ClinchStartDate:
+                    startTime.show();
+                    break;
+                case R.id.ClinchFinishDate:
+                    finishTime.show();
+                    break;
+                case R.id.ClinchInquire:
+                    startDate = helper.getMyDate(mtvStartTime.getText().toString());
+                    finishDate = helper.getMyDate(mtvFinishTime.getText().toString());
+                    if (!TextUtils.isEmpty(startDate) && !TextUtils.isEmpty(finishDate)) {
+                        String str = helper.compareTo(startDate, finishDate);
+                        int days = helper.daysBetween(startDate, finishDate);
+                        if (str.equalsIgnoreCase(startDate)) {
+                            mistakeDialog =  MistakeDialog.showDialog("请选择正确日期,起始日期不能超过截止日期", (Activity) mContext);
+                        } else if (days > 90) {
+                            mistakeDialog = MistakeDialog.showDialog("选择的日期间隔不能超过3个月", (Activity) mContext);
+                        } else {
+                            mDialog = LoadingDialog.initDialog((Activity) mContext, "正在查询...");
+                            mDialog.show();
+                            myList.clear();
+                            mAdapter.notifyDataSetChanged();
+                            refresh(position, "30", false);
+                        }
+                    }
+                    break;
+            }
+        }
+    }
+
     @Override
     public void callResult(ResultInfo info) {
+        if (mDialog != null)
+            mDialog.dismiss();
         String code = info.getCode();
         if(code.equals("0")){
-            myList = (List<StructuredFundEntity>)info.getData();
-            mAdapter.setData(myList);
-            listView.onRefreshComplete();
-        }else{
+            if (!mIsClean)
+                myList.clear();
+            Object object = info.getData();
+            if(object instanceof List){
+                myList = (List<StructuredFundEntity>)object;
+                position = myList.get(myList.size()-1).getPosition_str();
+                if(mIsClean)
+                    refresh += 30;
+                mAdapter.setData(myList);
+            }
+        }else if(code.equals("-6")){
+            skip.startLogin(mContext);
+        }else{//-1,-2,-3情况下显示定义好信息
             helper.showToast(mContext,info.getMsg());
             kong_null.setVisibility(View.GONE);
         }
+        listView.onRefreshComplete();
     }
 
     @Override
@@ -64,7 +165,7 @@ public class FJEntrustDealQueryPager extends BasePager implements InterfaceColle
     }
 
     @Override
-    public void callScall() {
+    public void callScall() {//回调底部最后一个item点击时滚动
         if(isScallBottom){
             listView.post(new Runnable() {
                 @Override
@@ -82,7 +183,7 @@ public class FJEntrustDealQueryPager extends BasePager implements InterfaceColle
             mAdapter.setCallback(this);
             listView.setAdapter(mAdapter);
             listView.setEmptyView(iv_isEmpty);
-            ifc.getData(20,this);
+            refresh("","30",false);
             listView.setOnRefreshListener(new PullToRefreshBase.OnRefreshListener<ListView>(){
                 @Override
                 public void onRefresh(PullToRefreshBase<ListView> refreshView) {
@@ -91,13 +192,13 @@ public class FJEntrustDealQueryPager extends BasePager implements InterfaceColle
                         listView.getLoadingLayoutProxy().setPullLabel("下拉刷新数据");
                         listView.getLoadingLayoutProxy().setReleaseLabel("释放开始刷新");
                         SystemClock.sleep(1500);
-                        ifc.getData(20,FJEntrustDealQueryPager.this);
+                        refresh("",String.valueOf(refresh),false);
                     }else if (listView.isShownFooter()){
                         listView.getLoadingLayoutProxy().setRefreshingLabel("正在刷新");
                         listView.getLoadingLayoutProxy().setPullLabel("上拉加载数据");
                         listView.getLoadingLayoutProxy().setReleaseLabel("释放开始刷新");
                         SystemClock.sleep(1500);
-                        ifc.getData(20,FJEntrustDealQueryPager.this);
+                        refresh(position,"30",true);
                     }
                 }
             });
@@ -125,9 +226,50 @@ public class FJEntrustDealQueryPager extends BasePager implements InterfaceColle
         }
     }
 
+    public void refresh(String page,String num,boolean isClean){
+        switch (getType()){
+            case 0://委托
+                if(TAG.equals("EntrustTodayPager")){
+                    ifc.queryTodayEntrust(mSession,page,num,"1",TAG,this);
+                }else if(TAG.equals("EntrustOneWeekPager")){
+                    ifc.queryHistoryEntrust(mSession,page,num,"1","","",TAG,this);
+                }else if(TAG.equals("EntrustInAMonthPager")){
+                    ifc.queryHistoryEntrust(mSession,page,num,"2","","",TAG,this);
+                }else if(TAG.equals("EntrustThreeWeekPager")){
+                    ifc.queryHistoryEntrust(mSession,page,num,"3","","",TAG,this);
+                }else if(TAG.equals("EntrustCustomPager")&&!TextUtils.isEmpty(startDate)&&!TextUtils.isEmpty(finishDate)){
+                    ifc.queryHistoryEntrust(mSession,page,num,"0",startDate,finishDate,TAG,this);
+                }
+                break;
+            case 1://成交
+                if(TAG.equals("DealTodayPager")){
+                    ifc.queryTodayDeal(mSession,page,num,TAG,this);
+                }else if(TAG.equals("DealOneWeekPager")){
+                    ifc.queryHistoryDeal(mSession,page,num,"1","","",TAG,this);
+                }else if(TAG.equals("DealInAMonthPager")){
+                    ifc.queryHistoryDeal(mSession,page,num,"2","","",TAG,this);
+                }else if(TAG.equals("DealThreeWeekPager")){
+                    ifc.queryHistoryDeal(mSession,page,num,"3","","",TAG,this);
+                }else if(TAG.equals("DealCustomPager")&&!TextUtils.isEmpty(startDate)&&!TextUtils.isEmpty(finishDate)){
+                    ifc.queryHistoryDeal(mSession,page,num,"0",startDate,finishDate,TAG,this);
+                }
+                break;
+        }
+        mIsClean = isClean;
+    }
+
+
     @Override
     public void destroy() {
         net.cancelSingleRequestByTag(TAG);
+        if(mistakeDialog!=null){
+            mistakeDialog.dismiss();
+            mistakeDialog = null;
+        }
+        if(mDialog!=null){
+            mDialog.dismiss();
+            mDialog = null;
+        }
         mAdapter.setCallback(null);
         mAdapter = null;
         myList.clear();
