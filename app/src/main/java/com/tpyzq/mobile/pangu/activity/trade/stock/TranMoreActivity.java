09decef88be382d066c188b3f9e89cc450c41c57
wangqi <1539977948@qq.com>
2017-06-28 13:52:32 +0800

    修改 分级基金弹窗

--- app/src/main/java/com/tpyzq/mobile/pangu/activity/trade/stock/TranMoreActivity.java
+++ app/src/main/java/com/tpyzq/mobile/pangu/activity/trade/stock/TranMoreActivity.java
@@ -53,10 +53,10 @@ public class TranMoreActivity extends BaseActivity implements AdapterView.OnItem
                 intent.putExtra("pageindex",TransactionLoginActivity.PAGE_INDEX_StructuredFundActivity);
                 intent.setClass(this,StructuredFundActivity.class);
                 break;
-            case 3:
-                intent.putExtra("pageindex",TransactionLoginActivity.PAGE_INDEX_NetworkVotingActivity);
-                intent.setClass(this,NetworkVotingActivity.class);
-                break;
+//            case 3:
+//                intent.putExtra("pageindex",TransactionLoginActivity.PAGE_INDEX_NetworkVotingActivity);
+//                intent.setClass(this,NetworkVotingActivity.class);
+//                break;
         }
         startActivity(intent);
     }
