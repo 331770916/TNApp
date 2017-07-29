c2fc8bc904c866aadb39bb48121bbd0acac77796
lixiong <lixiong@orbyun.com>
2017-07-29 10:27:42 +0800

    买卖界面搜索列表点击回调添加证券名称

--- app/src/main/java/com/tpyzq/mobile/pangu/interfac/StockItemCallBack.java
+++ app/src/main/java/com/tpyzq/mobile/pangu/interfac/StockItemCallBack.java
@@ -4,6 +4,6 @@ package com.tpyzq.mobile.pangu.interfac;
  * Created by 陈新宇 on 2016/10/13.
  */
 public interface StockItemCallBack {
-   void stockCode(String code);
+   void stockCode(String stockName, String code);
 
 }
