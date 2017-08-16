988535bbb83e3201085340a01d4a80ac81fe72da
zhangwenbo <331770916@qq.com>
2017-08-16 16:41:31 +0800

    自选股新闻接口替换

--- app/src/main/java/com/tpyzq/mobile/pangu/activity/market/selfChoice/childNews/SelfChoiceNewsTab.java
+++ app/src/main/java/com/tpyzq/mobile/pangu/activity/market/selfChoice/childNews/SelfChoiceNewsTab.java
@@ -3,6 +3,7 @@ package com.tpyzq.mobile.pangu.activity.market.selfChoice.childNews;
 import android.app.Activity;
 import android.app.Dialog;
 import android.content.Intent;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.FrameLayout;
@@ -10,11 +11,16 @@ import android.widget.ListView;
 import android.widget.Toast;
 
 import com.tpyzq.mobile.pangu.R;
+import com.tpyzq.mobile.pangu.activity.detail.newsTab.DetailNewsListActivity;
 import com.tpyzq.mobile.pangu.activity.home.information.NewsDetailActivity;
 import com.tpyzq.mobile.pangu.activity.market.BaseTabPager;
 import com.tpyzq.mobile.pangu.base.CustomApplication;
+import com.tpyzq.mobile.pangu.base.InterfaceCollection;
 import com.tpyzq.mobile.pangu.base.SimpleRemoteControl;
+import com.tpyzq.mobile.pangu.data.InformationEntity;
 import com.tpyzq.mobile.pangu.data.NewsInofEntity;
+import com.tpyzq.mobile.pangu.data.ResultInfo;
+import com.tpyzq.mobile.pangu.data.StockInfoEntity;
 import com.tpyzq.mobile.pangu.db.Db_HOME_INFO;
 import com.tpyzq.mobile.pangu.db.Db_PUB_STOCKLIST;
 import com.tpyzq.mobile.pangu.http.doConnect.self.SelfChoiceStockNews;
@@ -27,6 +33,7 @@ import com.tpyzq.mobile.pangu.view.dialog.LoadingDialog;
 import com.tpyzq.mobile.pangu.view.pulllayou.PullLayout;
 
 import java.util.ArrayList;
+import java.util.List;
 
 
 /**
@@ -34,18 +41,16 @@ import java.util.ArrayList;
  * 自选股新闻
  */
 public class SelfChoiceNewsTab extends BaseTabPager implements
-        AdapterView.OnItemClickListener, ICallbackResult, View.OnClickListener, PullLayout.OnPullCallBackListener {
+        AdapterView.OnItemClickListener, InterfaceCollection.InterfaceCallback, View.OnClickListener, PullLayout.OnPullCallBackListener {
 
-    private SimpleRemoteControl mSimpleRemoteControl;
     private static final String TAG = "SelfChoiceNewsTab";
     private SelfNewsAdapter mSelfNewsAdapter;
     private Activity mActivity;
     private Dialog mLoadDialog;
     private ArrayList<NewsInofEntity> mEntities;
     private PullLayout mPullLayout;
-    private int mPagerNum = 0;
+    private int mPagerNum = 1;
     private FrameLayout mKongLayout;
-    private boolean doReFrushing;
 
     public SelfChoiceNewsTab(Activity activity, ArrayList<BaseTabPager> tabs) {
         super(activity, tabs);
@@ -58,7 +63,6 @@ public class SelfChoiceNewsTab extends BaseTabPager implements
 
         mActivity = activity;
         mEntities = new ArrayList<>();
-        mSimpleRemoteControl = new SimpleRemoteControl(this);
         mPullLayout = (PullLayout) view.findViewById(R.id.selfchoiceNewPullDownScroll);
         mKongLayout = (FrameLayout) view.findViewById(R.id.stockNewsLayout);
         mKongLayout.setOnClickListener(this);
@@ -70,12 +74,48 @@ public class SelfChoiceNewsTab extends BaseTabPager implements
         mKongLayout.setVisibility(View.VISIBLE);
         requestSelfChoiceNews(String.valueOf(mPagerNum));
 //        initSelfChoiceNewsList(); //从数据库中取值
+
+
+    }
+
+    private String getStockCodeList() {
+        List<StockInfoEntity> datas = Db_PUB_STOCKLIST.queryStockListDatas();
+        if (datas != null && datas.size() > 0) {
+            StringBuilder sb1 = new StringBuilder();
+            for (int i = 0; i < datas.size(); i++) {
+                if (i < 10) {
+                    String stockNumber = datas.get(i).getStockNumber();
+                    if (!TextUtils.isEmpty(stockNumber) && !stockNumber.startsWith("10") && !stockNumber.startsWith("20")) {
+                        String _stockNumber = stockNumber;
+                        if (i == 9) {
+                            sb1.append(_stockNumber).append("");
+                        } else {
+                            sb1.append(_stockNumber).append(",");
+                        }
+                    }
+                }
+            }
+
+            String result = sb1.toString();
+
+            if (TextUtils.isEmpty(result) || result.length() <= 0) {
+                return "";
+            }
+
+            String reglex  = result.substring(result.length() - 1, result.length());
+            if (",".equals(reglex)) {
+                result = result.substring(0, result.length() - 1);
+            }
+
+            return result;
+        }
+
+        return "";
     }
 
     @Override
     public void onRefresh() {
-        mPagerNum = 0;
-        doReFrushing = true;
+        mPagerNum = 1;
         requestSelfChoiceNews(String.valueOf(mPagerNum));
     }
 
@@ -94,9 +134,9 @@ public class SelfChoiceNewsTab extends BaseTabPager implements
 
     private void requestSelfChoiceNews(String pagerNum) {
         int count = Db_PUB_STOCKLIST.getStockListCount();
-        if (mSimpleRemoteControl != null && count > 0) {
-            mSimpleRemoteControl.setCommand(new ToSelfChoiceStockNews(new SelfChoiceStockNews(TAG, pagerNum, null)));
-            mSimpleRemoteControl.startConnect();
+        String codes = getStockCodeList();
+        if (count > 0) {
+            mInterface.queryStockNews(codes,"30", pagerNum,TAG,this);
         } else {
             mEntities.clear();
             mSelfNewsAdapter.setDatas(mEntities);
@@ -138,33 +178,52 @@ public class SelfChoiceNewsTab extends BaseTabPager implements
     }
 
     @Override
-    public void getResult(Object result, String tag) {
-        if ("SelfChoiceStockNews".equals(tag)) {
-            mKongLayout.setVisibility(View.GONE);
-            if (mPullLayout != null) {
-                mPullLayout.finishPull();
-            }
-            if (mLoadDialog != null) {
-                mLoadDialog.dismiss();
-            }
-            if (null == result ||result instanceof String) {
-                if (mEntities == null || mEntities.size() <= 0 ) {
-                    mKongLayout.setVisibility(View.VISIBLE);
-                }
-//                CentreToast.showText(CustomApplication.getContext(), (String)result);
-                return;
-            }
-            ArrayList<NewsInofEntity> entities = (ArrayList<NewsInofEntity>) result;
-            if(entities != null && entities.size() > 0){
-                if(mPagerNum == 0){
-                    Db_HOME_INFO.deleteAllSelfNewsDatas();
-                    Db_HOME_INFO.addStockListDatas(entities);
-                    mEntities.clear();
-                }
-                mEntities.addAll(entities);
-            }
-            mSelfNewsAdapter.setDatas(mEntities);
+    public void callResult(ResultInfo info) {
+        mKongLayout.setVisibility(View.GONE);
+        if (mPullLayout != null) {
+            mPullLayout.finishPull();
         }
+        if (mLoadDialog != null) {
+            mLoadDialog.dismiss();
+        }
+        if (null == info.getData()) {
+            if (mEntities == null || mEntities.size() <= 0 ) {
+                mKongLayout.setVisibility(View.VISIBLE);
+            }
+//                CentreToast.showText(CustomApplication.getContext(), (String)result);
+            return;
+        }
+
+
+        List<InformationEntity> data = (List<InformationEntity>)info.getData();
+
+        if (data == null || data.size() <= 0) {
+            return;
+        }
+
+        ArrayList<NewsInofEntity> entities = new ArrayList<>();
+        for (InformationEntity informationEntity : data) {
+            NewsInofEntity newsInofEntity = new NewsInofEntity();
+            newsInofEntity.setId(informationEntity.getNewsno());
+            newsInofEntity.setComp(informationEntity.getSecuAbbr());
+            newsInofEntity.setTick(informationEntity.getSecuCode());
+            newsInofEntity.setDate(informationEntity.getTime());
+            newsInofEntity.setAuth(informationEntity.getSource());
+            newsInofEntity.setTitle(informationEntity.getTitle());
+            newsInofEntity.setSum(informationEntity.getDigest());
+            newsInofEntity.setStockCode(informationEntity.getSecuCode());
+            entities.add(newsInofEntity);
+        }
+
+        if(entities != null && entities.size() > 0){
+            if(mPagerNum == 0){
+                Db_HOME_INFO.deleteAllSelfNewsDatas();
+                Db_HOME_INFO.addStockListDatas(entities);
+                mEntities.clear();
+            }
+            mEntities.addAll(entities);
+        }
+        mSelfNewsAdapter.setDatas(mEntities);
     }
 
     @Override
